51a68cac05edd2c8508a748a0639a48f0a1b5572
rajkumar.hasgul <rajkumar.hasgul@kony.com>
2019-06-13 17:22:27 +0530

    added sdkinit to frm init

--- clientApp/breakingNews/studioactions/desktop/AS_Form_bc97732823dc4f9c83c0ca62930bac31.js
+++ clientApp/breakingNews/studioactions/desktop/AS_Form_bc97732823dc4f9c83c0ca62930bac31.js
@@ -1,1 +1,3 @@
-function AS_Form_bc97732823dc4f9c83c0ca62930bac31(eventobject) {}
+function AS_Form_bc97732823dc4f9c83c0ca62930bac31(eventobject) {
+    return sdkInit.call(this);
+}
--- clientApp/breakingNews/studioactions/desktop/AS_Form_c71e1d5f52fa41b78804badc75da6965.js
+++ clientApp/breakingNews/studioactions/desktop/AS_Form_c71e1d5f52fa41b78804badc75da6965.js
@@ -1,3 +1,1 @@
-function AS_Form_c71e1d5f52fa41b78804badc75da6965(eventobject) {
-    return sdkInit.call(this);
-}
+function AS_Form_c71e1d5f52fa41b78804badc75da6965(eventobject) {}
